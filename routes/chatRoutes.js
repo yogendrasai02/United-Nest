b3bc4cb5225ecd54fd94167e3e0be8cd6f33a2f2
avinash reddy <avinash150602@gmail.com>
2022-08-25 00:25:24 +0530

    modified chat feature

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -3,7 +3,7 @@ const chatController = require("../controllers/chatController");
 const chatRouter = express.Router();
 const authController = require("../controllers/authController.js");
 
-// chatRouter.use(authController.authenticate, authController.authorize('user'));
+chatRouter.use(authController.authenticate, authController.authorize('user'));
 
 chatRouter.get("/", chatController.chats_get);
 
@@ -13,6 +13,6 @@ chatRouter.get("/createGroup", chatController.group_get);
 
 chatRouter.post("/createGroup", chatController.group_post);
 
-chatRouter.get("/createChats", chatController.singlechat_post);
+// chatRouter.get("/createChats", chatController.singlechat_post);
 
 module.exports = chatRouter;
